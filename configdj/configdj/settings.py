571767b599b733a5689b99c95330b5421d7c3bae
Yarashayev Nurgeldi <nyarashbayev97@gmail.com>
2023-05-23 18:25:59 +0500

    Create new app

--- configdj/configdj/settings.py
+++ configdj/configdj/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'configdj.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'Practice',
+        'USER': 'root',
+        'PASSWORD': 'nurik0210',
+        'HOST':'localhost',
+        'PORT':'3306',
     }
 }
 
